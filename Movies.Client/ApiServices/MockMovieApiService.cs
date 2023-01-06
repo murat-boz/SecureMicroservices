e890829894b3b80b3c79f432dcc0c09642e7da42
murat-boz <muratboz@hotmail.com>
2023-01-06 16:50:24 +0300

    Added Movie.Client

---
+++ Movies.Client/ApiServices/MockMovieApiService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public class MockMovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var list = new List<Movie>();
+
+            list.Add(new Movie
+            {
+                Id          = 1,
+                Genre       = "Drama",
+                Title       = "The Shawshank Redemption",
+                Rating      = "9.3",
+                ImageUrl    = "images/src",
+                ReleaseDate = new DateTime(1994, 5, 5),
+                Owner       = "alice"
+            });
+
+            return list;
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,35 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public Task<IEnumerable<Movie>> GetMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
